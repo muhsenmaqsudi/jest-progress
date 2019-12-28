2f0a11b98b04e51c212e274f9f266f37a2598cf2
m.jafari <mohsen.jafari088@gmail.com>
2019-12-28 16:00:46 +0330

    add_triple_sum_test

--- src/__test__/sum.test.ts
+++ src/__test__/sum.test.ts
@@ -7,3 +7,7 @@ test("sum without args should be 0", () => {
 test("add 1 + 2 equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test("add 1 + 1 + 1 equal 3", () => {
+  expect(sum(1, 1, 1)).toBe(3);
+});
